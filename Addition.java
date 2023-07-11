b80dd1d00a13c1b90d4a5449e01971d1f80c795f
SujanaKavya <kavyahari16.gmail.com>
2023-07-11 10:28:56 -0400

    changes in AdditionFile TestBranch

--- Addition.java
+++ Addition.java
@@ -3,14 +3,14 @@ public class Addition
 {  
 public static void main(String args[])  
 {  
-int x, y, sum;  
+int x, y, sum1;  
 Scanner sc = new Scanner(System.in);  
 System.out.print("Enter the first number: ");  
 x = sc.nextInt();  
 System.out.print("Enter the second number: ");  
 y = sc.nextInt();  
-sum = sum(x, y);  
-System.out.println("The sum of two numbers x and y is: " + sum);  
+sum1 = sum(x, y);  
+System.out.println("The sum of two numbers x and y is: " + sum1);  
 }  
 //method that calculates the sum of 3 digits 
 public static int sum(int a, int b)  
