5246641f0b868417667d3360e939c3c6f18690f3
SujanaKavya <kavyahari16.gmail.com>
2023-07-12 14:42:54 -0400

    Changes of Add File by Test Branch

--- Addition.java
+++ Addition.java
@@ -5,7 +5,7 @@ public static void main(String args[])
 {  
 int x, y, sum3;  
 Scanner sc = new Scanner(System.in);  
-System.out.print("Enter the first number: ");  
+System.out.print("Enter the first integer number: ");  
 x = sc.nextInt();
 System.out.print("Enter the second number: ");  
 y = sc.nextInt();  
